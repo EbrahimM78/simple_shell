d5df73eae94b09ab5f3c815d206bcfe5fbb1e7fc
Ebrahim <122838783+EbrahimM78@users.noreply.github.com>
2023-05-23 20:36:46 +0200

    Files added

--- _putchar.c
+++ _putchar.c
@@ -1,13 +1,19 @@
+#include "main.h"
 #include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
+#include <stddef.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * _putchar - | putchar
+ * @c: var
+ * Return: var
  */
+
 int _putchar(char c)
 {
-  return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
